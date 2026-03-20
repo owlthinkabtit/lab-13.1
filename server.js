1368a8ed6df84187365c116afda9f1e5a974c172
Meek Meek <haggins.jameka@gmail.com>
2026-03-20 13:20:21 -0400

    Coming back to working on this thing.

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ import express from 'express'
 import 'dotenv/config'
 import {MongoClient} from 'mongodb'
 
+const app = express()
+const port = 3001
 const connectionString = process.env.MONGO_URI
 const client = new MongoClient(connectionString)
-const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  try {
+    await client
+  }
 })
 
 async function run() {
@@ -22,7 +25,6 @@ async function run() {
 
 run().catch(console.dir);
 
-const port = 3000
 
 app.listen(port, () => {
   console.log('Listening on port:' + port)
